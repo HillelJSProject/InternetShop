880dd21bf4d635ab98acc8cea6ddcdc7abfbc5c5
stanislavk1982 <stanislavk.1982@gmail.com>
2017-08-13 23:26:02 +0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+let gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    less = require('gulp-less'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssmin = require('gulp-clean-css'),
+    jsmin = require('gulp-uglify'),
+    webserver = require('gulp-webserver'),
+    livereload = require('gulp-livereload');
+
+
+
+
+//gulp.task('default', ['build']);
+
+
